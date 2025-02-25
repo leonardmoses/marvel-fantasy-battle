f155b26dff141b95ef74e64cb884c92811813954
Leonard Moses <leonardsamuelmoses@gmail.com>
2025-02-25 02:32:53 -0500

    Added Link Game Board

---
+++ src/app/gameboard/page.js
@@ -0,0 +1,9 @@
+const GameBoard = () => {
+    return (
+        <div>
+            <h1>Game Board</h1>
+        </div>
+    );
+}
+
+export default GameBoard;
--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ const MarvelFantasyBattleHome = () => {
           <h2 className="text-transparent bg-clip-text bg-gradient-to-r from-Secondary to-MarvelRed text-6xl text-center">
             Fantasy Battle
           </h2>
-          <Link href="">
+          <Link href="./gameboard">
             <h3 className="text-white text-center text-2xl hover:text-MarvelRed active:text-Secondary animate-pulse hover:animate-grow ">Enter</h3>
           </Link>
         </div>
