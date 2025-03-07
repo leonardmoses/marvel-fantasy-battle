7b74bdc2fce92967268b3fd08fe4ef8a7f63cb18
Leonard Moses <leonardsamuelmoses@gmail.com>
2025-03-07 14:47:04 -0500

    Added Dice Image and imported into Gameboard.

--- src/app/gameboard/client.js
+++ src/app/gameboard/client.js
@@ -3,9 +3,9 @@ import Image from "next/image";
 const GameboardClient = () => {
     return (
         <main className="">
-
-
-
+            <div className="w-fit ml-auto mr-20">
+                <Image src="/images/Dice.png" alt="alt" width={200} height={200} />
+            </div>
 
         </main>
     );
